4ceb6ceee39e7dbc4c50e1ca471e7e054aedd73b
gujunmin <3013366498@qq.com>
2017-05-20 16:57:07 +0800

    update 170520 16:57

--- scripts/imgHover.js
+++ scripts/imgHover.js
@@ -1,14 +1,16 @@
 $(function() {
+    // 添加span元素
     $('#jnBrandList li').each(function() {
-        let $img = $(this).find('img');
-        let img_w = $img.width();
-        let img_h = $img.height();
+        let img = $(this).find('img');
+        let img_w = img.width();
+        let img_h = img.height();
         let spanHtml = `
             <span style="position: absolute; top: 0; left: 6px; width: ${img_w}px; height: ${img_h}px;" class="imageMask"></span>
         `;
         $(spanHtml).appendTo(this);
     });
 
+    // 为 span 元素绑定 hover 实现，然后触发.imageOver class的交替
     $('#jnBrandList .imageMask').hover(function() {
         $(this).toggleClass('imageOver');
     });
